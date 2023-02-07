6048e794613bb4c4d742e438ca5db2a37de38a46
zds-dev <adstronach@gmail.com>
2023-02-07 21:17:36 +0000

    Completed removal of circular imports & fixed up import list.

--- personalfinancetoolbox/models/__init__.py
+++ personalfinancetoolbox/models/__init__.py
@@ -1,11 +1,7 @@
 # Import base abstract class
 from .ValueHolding import ValueHolding
 
-# TODO: Make sure circular imports are not a problem need some architecture changes
-
 # Import subclasses of abstract
 from .Asset import Asset
 from .CombinedAsset import CombinedAsset
 from .InterestAccruingAsset import YearlyInterestAccruing
-
-# Import helper functions

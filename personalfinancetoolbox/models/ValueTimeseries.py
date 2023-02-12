851072533e051289f0d4463a8860ad0b1441f69a
zds-dev <adstronach@gmail.com>
2023-02-12 12:18:59 +0000

    Started working on ValueTimeseries.py (abstraction of UserDict) for the Value of an asset to allow restrictions to the value, and assertion that it follows expected form.
    UserDict vs MutableMapping? Not sure what is best here.

---
+++ personalfinancetoolbox/models/ValueTimeseries.py
@@ -0,0 +1,33 @@
+from collections import UserDict
+from datetime import datetime
+
+
+class ValueTimeseries(UserDict):
+    def __setitem__(self, key, value):
+        # Assert key is datetime and value is number
+        if not isinstance(key, datetime):
+            raise TypeError('Key must be a datetime')
+        if not isinstance(value, (int, float)):
+            raise TypeError('Value must be a number')
+        super().__setitem__(key, value)
+
+
+class ValueLimitedTimeseries(ValueTimeseries):
+    def __init__(self, *args, min_value=None, max_value=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if min_value is None:
+            min_value = -float('inf')
+        if max_value is None:
+            max_value = float('inf')
+        self._min_value = min_value
+        self._max_value = max_value
+
+    def __setitem__(self, key, value):
+        if value > self._max_value:
+            raise ValueError('Value cannot be greater than {}'.format(self._max_value))
+        if value < self._min_value:
+            raise ValueError('Value cannot be less than {}'.format(self._min_value))
+        super().__setitem__(key, value)
+
+
+
